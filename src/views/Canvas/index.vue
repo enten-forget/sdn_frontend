18f286150e8dd1d6994bb31a4b3d6cbbfee9e610
遠天 <enten0000@gmail.com>
2022-06-25 09:16:40 +0800

    change the network icon

--- src/views/Canvas/index.vue
+++ src/views/Canvas/index.vue
@@ -2,18 +2,6 @@
   <div class="canvas-container">
     <LoadingSpinner v-if="loading !== false" />
     <template v-else>
-<!--      <template class="opera-btn">-->
-<!--        <v-navigation-drawer-->
-<!--          absolute-->
-<!--          right-->
-<!--          hide-overlay-->
-<!--          permanent-->
-<!--          stateless-->
-<!--          temporary-->
-<!--          style="width: 200px"-->
-<!--        >-->
-<!--        </v-navigation-drawer>-->
-<!--      </template>-->
       <template class="ryu">
         <v-navigation-drawer
           v-model="showPanel"
@@ -46,17 +34,27 @@
       </template>
       <v-row style="height: 100%">
         <v-col cols="9">
-          <v-card style="margin: 10px; height: 97%; border: 1px solid grey">
+          <v-card
+            style="
+              margin: 10px 0 10px 10px;
+              height: 97%;
+              border: 1px solid grey;
+            "
+          >
             <VisContainer ref="vis" @edit-item="editItem" />
           </v-card>
         </v-col>
         <v-col cols="3">
-          <v-card style="margin-top: 10px; height: 97%; border: 1px solid grey">
+          <v-card
+            style="
+              margin: 10px 10px 10px 0;
+              height: 97%;
+              border: 1px solid grey;
+            "
+          >
             <v-expansion-panels>
               <v-expansion-panel>
-                <v-expansion-panel-header>
-                  网络构建
-                </v-expansion-panel-header>
+                <v-expansion-panel-header> 网络构建</v-expansion-panel-header>
                 <v-expansion-panel-content>
                   <v-list-item>
                     <div class="topo-div">
@@ -69,7 +67,7 @@
                       >
                         <v-icon>$vuetify.icons.net-edge</v-icon>
                       </v-btn>
-                      <p class="ma-0 font-weight-thin text-sm-body-2">Edge</p>
+                      <p class="ma-0 font-weight-thin text-sm-body-2">线路</p>
                     </div>
                     <div class="topo-div">
                       <v-btn
@@ -81,7 +79,7 @@
                       >
                         <v-icon>$vuetify.icons.net-port</v-icon>
                       </v-btn>
-                      <p class="ma-0 font-weight-thin text-sm-body-2">Port</p>
+                      <p class="ma-0 font-weight-thin text-sm-body-2">端口</p>
                     </div>
                   </v-list-item>
                   <v-list-item>
@@ -95,7 +93,7 @@
                       >
                         <v-icon>$vuetify.icons.net-host</v-icon>
                       </v-btn>
-                      <p class="ma-0 font-weight-thin text-sm-body-2">Host</p>
+                      <p class="ma-0 font-weight-thin text-sm-body-2">主机</p>
                     </div>
                     <div class="topo-div">
                       <v-btn
@@ -107,7 +105,7 @@
                       >
                         <v-icon>$vuetify.icons.net-switch</v-icon>
                       </v-btn>
-                      <p class="ma-0 font-weight-thin text-sm-body-2">Switch</p>
+                      <p class="ma-0 font-weight-thin text-sm-body-2">交换机</p>
                     </div>
                   </v-list-item>
                   <v-list-item>
@@ -121,7 +119,7 @@
                       >
                         <v-icon>$vuetify.icons.net-controller</v-icon>
                       </v-btn>
-                      <p class="ma-0 font-weight-thin text-sm-body-2">Controller</p>
+                      <p class="ma-0 font-weight-thin text-sm-body-2">控制器</p>
                     </div>
                     <div class="topo-div">
                       <v-btn
@@ -133,7 +131,7 @@
                       >
                         <v-icon>$vuetify.icons.net-label</v-icon>
                       </v-btn>
-                      <p class="ma-0 font-weight-thin text-sm-body-2">Label</p>
+                      <p class="ma-0 font-weight-thin text-sm-body-2">标签</p>
                     </div>
                   </v-list-item>
                   <v-list-item>
@@ -147,27 +145,27 @@
                       >
                         <v-icon>mdi-delete</v-icon>
                       </v-btn>
-                      <p class="ma-0 font-weight-thin text-sm-body-2">Delete</p>
+                      <p class="ma-0 font-weight-thin text-sm-body-2">删除</p>
                     </div>
                   </v-list-item>
                 </v-expansion-panel-content>
               </v-expansion-panel>
               <v-expansion-panel>
-                <v-expansion-panel-header>
-                  提交
-                </v-expansion-panel-header>
+                <v-expansion-panel-header> 提交</v-expansion-panel-header>
                 <v-expansion-panel-content>
                   <v-list-item>
                     <div class="topo-div">
-                      <v-btn :disabled="working" color="primary" @click="downloadScript"
-                      >SUBMIT
+                      <v-btn
+                        :disabled="working"
+                        color="primary"
+                        @click="downloadScript"
+                        >SUBMIT
                       </v-btn>
                     </div>
                   </v-list-item>
                 </v-expansion-panel-content>
               </v-expansion-panel>
             </v-expansion-panels>
-
           </v-card>
         </v-col>
       </v-row>
